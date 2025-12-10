368ff2f9abeaf499892707cb4e70fe313c76183e
ishansainii <ishan21465@iiitd.ac.in>
2025-12-10 21:06:15 +0530

    some changes

---
+++ models/commentModel.js
@@ -0,0 +1,25 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+ 
+
+// route handlers
+const commentSchema =  new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "Post", // Reference to the post model
+    },
+    user: {
+        type:String,
+        required: true,
+    },
+    body: {
+        type:String,
+        required: true,
+    }
+
+});
+
+
+// export
+module.exports = mongoose.model("Comment", commentSchema)
---
+++ routes/blog.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// import controller
+const {dummyLink} = require("../controllers/LikeController")
+
+
+// mapping create
+
+
+// export
